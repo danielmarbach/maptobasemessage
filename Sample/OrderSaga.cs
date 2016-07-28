93a390785ceca5aaa44b7226341554c4bfa67daf
Simon Cropp <simon.cropp@gmail.com>
2016-07-29 09:21:13 +1000

    init

---
+++ Sample/OrderSaga.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+
+#region thesaga
+public class OrderSaga :
+    Saga<OrderSagaData>,
+    IAmStartedByMessages<StartOrder>,
+    IHandleMessages<CompleteOrder>
+{
+    static ILog log = LogManager.GetLogger<OrderSaga>();
+
+    protected override void ConfigureHowToFindSaga(SagaPropertyMapper<OrderSagaData> mapper)
+    {
+        mapper.ConfigureMapping<StartOrder>(message => message.OrderId)
+                .ToSaga(sagaData => sagaData.OrderId);
+        mapper.ConfigureMapping<CompleteOrder>(message => message.OrderId)
+                .ToSaga(sagaData => sagaData.OrderId);
+    }
+
+    public Task Handle(StartOrder message, IMessageHandlerContext context)
+    {
+        Data.OrderId = message.OrderId;
+        log.Info($"Saga with OrderId {Data.OrderId} received StartOrder with OrderId {message.OrderId}");
+        var completeOrder = new CompleteOrder
+        {
+            OrderId = Data.OrderId
+        };
+        return context.SendLocal(completeOrder);
+    }
+
+    public Task Handle(CompleteOrder message, IMessageHandlerContext context)
+    {
+        log.Info($"Saga with OrderId {Data.OrderId} received CompleteOrder with OrderId {message.OrderId}");
+        MarkAsComplete();
+        return Task.FromResult(0);
+    }
+
+}
+#endregion
